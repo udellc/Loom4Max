64cf43c60bb1e63da791e19f5ffe93528e13407a
Ethan Barker <ethanbarker0@gmail.com>
2023-04-16 15:05:51 -0700

    fix updateTimeData

--- Loom/javascript/mongoconnect1.js
+++ Loom/javascript/mongoconnect1.js
@@ -8,6 +8,7 @@ let device = null;
 
 function outletDocument(doc) {
   delete doc._id; // TODO: Why do we have to delete these?
+  delete doc.date;
   doc.ID = { instance: device };
   const data = JSON.stringify(doc);
   maxApi.outlet(data);
@@ -18,7 +19,6 @@ function updateData(count) {
   if (!collection || !device) return;
   const cursor = collection.find().sort({ $natural: -1 });
   if (count !== undefined) cursor.limit(count);
-  maxApi.post(cursor.length);
   cursor.forEach((doc) => {
     outletDocument(doc, device);
   });
@@ -26,21 +26,24 @@ function updateData(count) {
 
 function updateTimeData(startTime, endTime) {
   if (!collection || !device) return;
-  var STPacket = collection.findOne( { "Timestamp.time_local": startTime } );     //Find packet with specified start time
-  maxApi.post(STPacket)
-  var ETPacket = collection.findOne( { "Timestamp.time_local": endTime } );       //Do the same for end time
-  maxApi.post(ETPacket)
-  var STNum = STPacket.Packet.Number;                                             //Store the packet number of start time packet
-  var ETNum = ETPacket.Packet.Number;
-
-  var packs = collection.find( {                                              //store the packets in packs
-    $and: [                                                                   //Find the packet count for all packets
-      { Packet: { Number: { $lte: ETNum } } },                                //with a timestamp before/equal to the end time
-      { Packet: { Number: { $gte: STNum } } }                                 //and after/equal to the start time
-    ]
-  } );
-  maxApi.post(packs.length);                                                
-  packs.forEach((doc) => {                                                    //outlet the data the same way
+  var cursor = collection.aggregate([
+    {
+      $addFields: {
+        date: {
+          $toDate: "$Timestamp.time_local"
+        }
+      }
+    },
+    {
+      $match: {
+        date: {
+          $gte: new Date(startTime),
+          $lte: new Date(endTime),
+        }
+      }
+    }
+  ])
+  cursor.forEach((doc) => {                                                    //outlet the data the same way
     outletDocument(doc, device);                                              //as the updateData function
   });
 }
@@ -79,9 +82,9 @@ maxApi.addHandler('getLast', (packetCount, mongoUsername, mongoPassword, mongoUn
 });
 //Pulls packets via a specified start and end time
 maxApi.addHandler('getByTime', (startTime, endTime, mongoUsername, mongoPassword, mongoUniqueClusterVariable, mongoDatabase, newDevice) => {
-  	startTime = startTime.replace("T"," ")
-	endTime = endTime.replace("T"," ")
-	
+  startTime = startTime.replace("T"," ")
+  endTime = endTime.replace("T"," ")
+
   if (!collection || !device) {
     run(mongoUsername, mongoPassword, mongoUniqueClusterVariable, mongoDatabase, newDevice)
       .then(() => updateTimeData(startTime, endTime));
